3e551003a32c5a0f6579ae0aa1951248dc020e73
Frederik Bertelsen <frbb@itu.dk>
2024-04-17 11:04:10 +0200

    fix fomratting

--- WordWord/MultiSet.fs
+++ WordWord/MultiSet.fs
@@ -1,58 +1,50 @@
 module MultiSet
 
-    type MultiSet<'a when 'a : comparison> = R of Map<'a, uint32> 
+type MultiSet<'a when 'a: comparison> = R of Map<'a, uint32>
 
-    let empty = R Map.empty
+let empty = R Map.empty
 
-    let isEmpty  (R s : MultiSet<'a>) =   Map.isEmpty s
+let isEmpty (R s: MultiSet<'a>) = Map.isEmpty s
 
-    let size (R s : MultiSet<'a>) =
-        if Map.isEmpty s then
-            0u
-        else
-            (Map.fold (fun acc a u ->( acc + u)) 0u s)
-    
-    let contains (key : 'a) (R s : MultiSet<'a>) = Map.containsKey key s
+let size (R s: MultiSet<'a>) =
+    if Map.isEmpty s then
+        0u
+    else
+        (Map.fold (fun acc a u -> (acc + u)) 0u s)
 
-    let numItems (key : 'a) (R s : MultiSet<'a>) =
-        if Map.containsKey key s then
-            s.[key]
-        else
-            0u
+let contains (key: 'a) (R s: MultiSet<'a>) = Map.containsKey key s
 
-    let add (e : 'a) (n : uint32) (R m : MultiSet<'a>) : MultiSet<'a> = R (Map.add e (if  m.ContainsKey e then (Map.find e m) + n else n) m)
+let numItems (key: 'a) (R s: MultiSet<'a>) =
+    if Map.containsKey key s then s.[key] else 0u
 
-    let addSingle (e : 'a) (m : MultiSet<'a>) : MultiSet<'a> = add e 1u m
-    
-    let remove (e : 'a) (n : uint32) (R m  : MultiSet<'a>) : MultiSet<'a> =
-        let o = Option.defaultValue 0u (Map.tryFind e m)
-        if m.ContainsKey e then
-            if o > n then
-                R (Map.add e (o-n) m)
-            else
-                R (Map.remove e m)
-        else
-            R m
+let add (e: 'a) (n: uint32) (R m: MultiSet<'a>) : MultiSet<'a> =
+    R(Map.add e (if m.ContainsKey e then (Map.find e m) + n else n) m)
 
-    let removeSingle (e : 'a) (m : MultiSet<'a>) : MultiSet<'a>= remove e 1u m
+let addSingle (e: 'a) (m: MultiSet<'a>) : MultiSet<'a> = add e 1u m
+
+let remove (e: 'a) (n: uint32) (R m: MultiSet<'a>) : MultiSet<'a> =
+    let o = Option.defaultValue 0u (Map.tryFind e m)
+
+    if m.ContainsKey e then
+        if o > n then R(Map.add e (o - n) m) else R(Map.remove e m)
+    else
+        R m
+
+let removeSingle (e: 'a) (m: MultiSet<'a>) : MultiSet<'a> = remove e 1u m
 
 
-    let fold (F : 'b -> 'a -> uint32 -> 'b) (acc : 'b) (R s : MultiSet<'a>) =
-        Map.fold F acc s
-    let foldBack (F : 'a -> uint32 -> 'b -> 'b) (R s : MultiSet<'a>) (acc : 'b) =
-        Map.foldBack F s acc
-    
-    let ofList (_ : 'a list) : MultiSet<'a> =  empty
-    let toList (_ : MultiSet<'a>) : 'a list = []
+let fold (F: 'b -> 'a -> uint32 -> 'b) (acc: 'b) (R s: MultiSet<'a>) = Map.fold F acc s
+let foldBack (F: 'a -> uint32 -> 'b -> 'b) (R s: MultiSet<'a>) (acc: 'b) = Map.foldBack F s acc
+
+let ofList (_: 'a list) : MultiSet<'a> = empty
+let toList (_: MultiSet<'a>) : 'a list = []
 
 
-    let map (_ : 'a -> 'b) (_ : MultiSet<'a>) : MultiSet<'b> = empty
+let map (_: 'a -> 'b) (_: MultiSet<'a>) : MultiSet<'b> = empty
 
-    let union (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = empty
-    let sum (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = empty
-    
-    let subtract (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = empty
-    
-    let intersection (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = empty
-       
-    
+let union (_: MultiSet<'a>) (_: MultiSet<'a>) : MultiSet<'a> = empty
+let sum (_: MultiSet<'a>) (_: MultiSet<'a>) : MultiSet<'a> = empty
+
+let subtract (_: MultiSet<'a>) (_: MultiSet<'a>) : MultiSet<'a> = empty
+
+let intersection (_: MultiSet<'a>) (_: MultiSet<'a>) : MultiSet<'a> = empty
